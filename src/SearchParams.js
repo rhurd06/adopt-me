746d89ff779ac25b2e834955eb0d7e59c2663bfb
Robyn Noble <robynhurd09@gmail.com>
2021-08-24 15:41:10 -0500

    introducing hooks, useState

---
+++ src/SearchParams.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+    const location = "Seattle, WA";
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" value={location}
+                    placeholder="Location" />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams;
