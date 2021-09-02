47173401db2932f8ad80a7eac73a63fa3968d55a
Robyn Noble <robynhurd09@gmail.com>
2021-09-02 08:58:30 -0500

    added pets api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { async } from "fast-glob";
+import { useState, useEffect } from "react";
+import Pet from './pet';
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile" ];
 
 const SearchParams = () => {
-
-    const [location, setLocation] = useState("Seattle, WA");
+    const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
+    const [pets, setPets] = useState([]);
     const breeds = [];
 
+useEffect(() => {
+    requestPets();
+}, []); //eslint-disable-line react-hooks/exhaustive-deps
 
+async function requestPets() {
+    const res = await fetch(
+        `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+}
 
     return (
         <div className="search-params">
@@ -58,6 +71,13 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
+            {
+                pets.map(pet => (
+                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed}
+                    key={pet.id}
+                    />
+                ))
+            }
         </div>
     )
 }
