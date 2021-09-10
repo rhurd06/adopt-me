1a8e77c264a46b9130de8a2cbcc09619f31d0bc9
Robyn Noble <robynhurd09@gmail.com>
2021-09-10 12:47:56 -0500

    fixed redirect to work on first render if error occurs

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -8,12 +8,7 @@ class ErrorBoundary extends Component{
     }
     componentDidCatch(error, info) {
         console.error("ErrorBoundary caught an error", error, info);
-    }
-
-    componentDidUpdate() {
-        if (this.state.hasError) {
-            setTimeout(() => this.setState({ redirect: true}), 5000)
-        }
+        setTimeout(() => this.setState({ redirect: true}), 5000)
     }
 
     render () {
